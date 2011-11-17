d7716db2d4760339d36ad96e432837b032891e7b
Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
2011-11-17 20:42:30 +0100

    Initialize bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Funstaff\FeedBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * Configuration
+ *
+ * @author Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('funstaff_feed');
+
+        return $treeBuilder;
+    }
+}
---
+++ FunstaffFeedBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Funstaff\FeedBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FunstaffFeedBundle
+ *
+ * @author Bertrand Zuchuat <bertrand.zuchuat@gmail.com>
+ */
+class FunstaffFeedBundle extends Bundle
+{
+}
